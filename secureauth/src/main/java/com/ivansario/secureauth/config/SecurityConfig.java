854ecda6897d3dd5ffc3267d8a55c9e51f8b8b95
ivansmdev <ivan.sario.madrigal@gmail.com>
2026-05-04 20:43:47 +0200

    Add SecurityConfig class to configure security settings and HTTP request authorization

---
+++ secureauth/src/main/java/com/ivansario/secureauth/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.ivansario.secureauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http.csrf(csrf -> csrf.disable())
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        )
+        .authorizeHttpRequests(
+            auth -> auth.requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated())
+        .build();
+    }
+
+}
