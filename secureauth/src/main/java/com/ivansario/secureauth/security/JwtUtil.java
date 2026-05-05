15687263286ffcdb3c1bec3dee3480d1511a4c3b
ivansmdev <ivan.sario.madrigal@gmail.com>
2026-05-05 22:06:36 +0200

    Remove @Component annotation from JwtUtil class to simplify configuration

--- secureauth/src/main/java/com/ivansario/secureauth/security/JwtUtil.java
+++ secureauth/src/main/java/com/ivansario/secureauth/security/JwtUtil.java
@@ -1,9 +1,7 @@
 package com.ivansario.secureauth.security;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-@Component
 public class JwtUtil {
 
     @Value("${JWT_SECRET_KEY}")
