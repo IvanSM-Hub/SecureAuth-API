ffa22e1c82b616df2c8cafae9a95b15e636b5b94
Iván Sarió Madrigal <isario@odec.es>
2026-06-09 10:15:41 +0200

    feat: security configuration for production environment

--- secureauth/src/main/java/com/ivansario/secureauth/config/SecurityConfig.java
+++ secureauth/src/main/java/com/ivansario/secureauth/config/SecurityConfig.java
@@ -52,6 +52,7 @@ public class SecurityConfig {
         .authorizeHttpRequests(
             auth -> auth
             .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+            .requestMatchers("/actuator/health").permitAll()
             .requestMatchers(
                 "/api/auth/login",
                 "/api/user/register",
